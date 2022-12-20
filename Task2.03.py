fe10e4b3f1ab8cc1e3e736bb1aab4d7000697465
Вероника Акулова <fenek1901@gmail.com>
2022-12-20 18:56:04 +0300

    добавлены варианты решений с семинара

--- Task2.03.py
+++ Task2.03.py
@@ -21,3 +21,14 @@ n_list = [(round((1+1/i)**i, 2)) for i in range(1, n+1)]
 
 print(n_list)
 print(round(sum(n_list), 2))
+
+# вариант с семинара
+print('Задайте список из n чисел последовательности (1+1/n)^n и выведите на экран их сумму.')
+n = int(input('Введите число N:-> '))
+seq = dict()
+seq_sum = 0
+for i in range(1, n+1):
+    seq[i] = round((1+1/i)**i, 2)
+print(f'Для N={n} {seq}')
+print(f'Сумма {sum(seq.values())}')
+# От Евгений всем 10:28 PM
--- Task2.05.py
+++ Task2.05.py
@@ -19,3 +19,19 @@ for i in range(len(my_list)-1, 0, -1):
     j = random.randint(0, i + 1)
     my_list[i], my_list[j] = my_list[j], my_list[i]
 print(my_list)
+
+
+# вариант с семинара
+
+def list_shuffle(_list: list):
+    shuffled_list = []
+    temp_list = _list
+
+    for _ in range(len(_list)):
+        position = random.randint(0, len(temp_list) - 1)
+        shuffled_list.append(temp_list.pop(position))
+    return shuffled_list
+
+
+print(list_shuffle([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]))
+
